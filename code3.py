69341f1f3e6aeae7a75c21d80da0df81bebb9051
AJITH KUMAR P J K <pjkajithkumar@gmail.com>
2021-11-16 17:01:12 +0530

    Revert "modified commit"
    This reverts commit 1f07e970cf49e5c086b6bac886c58a4ec6bbdc59.

--- code3.py
+++ code3.py
@@ -422,7 +422,8 @@ menubar = Menu(calc)
 # ManuBar 1 :
 filemenu = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label = 'File', menu = filemenu)
-
+filemenu.add_command(label = "Standard", command = Standard)
+filemenu.add_command(label = "Scientific", command = Scientific)
 filemenu.add_separator()
 filemenu.add_command(label = "Exit", command = iExit)
 
